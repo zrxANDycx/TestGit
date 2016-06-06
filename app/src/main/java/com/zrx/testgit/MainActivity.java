02673b0086e64c76f18c57026e4e9a2acbf0dc97
zrxANDycx <zrx1358516864@163.com>
2016-06-06 15:21:33 +0800

    又一次连接github

--- app/src/main/java/com/zrx/testgit/MainActivity.java
+++ app/src/main/java/com/zrx/testgit/MainActivity.java
@@ -2,9 +2,10 @@ package com.zrx.testgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
